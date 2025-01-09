9bd7f9b7d4328f137026cd5b45f0e80d3f9acd05
AhmedDarwish74 <132007616+AhmedDarwish74@users.noreply.github.com>
2025-01-09 10:33:53 +0200

    serializers.py

--- fitness_tracker_api/activities/serializers.py
+++ fitness_tracker_api/activities/serializers.py
@@ -1,34 +1,32 @@
 from rest_framework import serializers
-from .models import User, Activity
+from .models import Exercise, User, Activity
+
+# Serializer for the Exercise model
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = '__all__'
+
+    def validate_difficulty(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError("Difficulty must be between 1 and 10.")
+        return value
 
 # Serializer for the User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']  # Fields to include
-        extra_kwargs = {'password': {'write_only': True}}  # Ensure password is write-only
-
-    def create(self, validated_data):
-        # Create a new user with the validated data, using create_user to ensure password hashing
-        user = User.objects.create_user(**validated_data)
-        return user
+        fields = ['id', 'username', 'email']
 
 # Serializer for the Activity model
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = ['id', 'user', 'activity_type', 'duration', 'distance', 'calories_burned', 'date']  # Fields to include
-        read_only_fields = ['user']  # Ensure user field is read-only
+        fields = '__all__'
 
     def validate(self, data):
-        # Custom validation for duration and calories burned
-        if data['duration'] <= 0:
-            raise serializers.ValidationError("Duration must be greater than 0.")
-        if data['calories_burned'] <= 0:
-            raise serializers.ValidationError("Calories burned must be greater than 0.")
-        
-        # Additional validation for Weightlifting: Distance should not be provided
-        if data['activity_type'] == 'Weightlifting' and data.get('distance'):
+        if data['activity_type'] == 'Weightlifting' and data.get('distance') is not None:
             raise serializers.ValidationError("Distance should not be provided for Weightlifting activities.")
-        
+        if data['date'] > date.today():
+            raise serializers.ValidationError("Activity date cannot be in the future.")
         return data
