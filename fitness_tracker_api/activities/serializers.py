9ff25001a18e2c8a4ded635257f4d1c19de9a0e5
AhmedDarwish74 <132007616+AhmedDarwish74@users.noreply.github.com>
2025-01-09 12:19:44 +0200

    serializers.py

--- fitness_tracker_api/activities/serializers.py
+++ fitness_tracker_api/activities/serializers.py
@@ -1,32 +1,30 @@
 from rest_framework import serializers
-from .models import Exercise, User, Activity
+from .models import Exercise, Activity
+from datetime import date
 
 # Serializer for the Exercise model
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the Exercise model
 
     def validate_difficulty(self, value):
+        # Validate that the difficulty is between 1 and 10
         if value < 1 or value > 10:
             raise serializers.ValidationError("Difficulty must be between 1 and 10.")
         return value
-
-# Serializer for the User model
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
 
 # Serializer for the Activity model
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from the Activity model
 
     def validate(self, data):
+        # Validate that distance is not provided for Weightlifting activities
         if data['activity_type'] == 'Weightlifting' and data.get('distance') is not None:
             raise serializers.ValidationError("Distance should not be provided for Weightlifting activities.")
+        # Validate that the activity date is not in the future
         if data['date'] > date.today():
             raise serializers.ValidationError("Activity date cannot be in the future.")
         return data
