563dfb3f5e4b6c244be335bc6dca2decc0b431fc
AhmedDarwish74 <132007616+AhmedDarwish74@users.noreply.github.com>
2025-01-09 10:36:35 +0200

    urls.py

--- fitness_tracker_api/activities/urls.py
+++ fitness_tracker_api/activities/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import ActivityViewSet, ActivityMetricsView
+from .views import ActivityViewSet, ActivityMetricsView, ExerciseViewSet
 from rest_framework.routers import DefaultRouter
 
 # Create a router instance
 router = DefaultRouter()
+
 # Register the ActivityViewSet with the router
 router.register(r'activities', ActivityViewSet, basename='activity')
+
+# Register the ExerciseViewSet with the router
+router.register(r'exercises', ExerciseViewSet, basename='exercise')
 
 # Define urlpatterns for the app
 urlpatterns = [
